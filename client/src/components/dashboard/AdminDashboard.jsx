4012f132239d26abfe9076f9dbb11e21761b990d
muhammedAbdellayassin <ssswar367@gmail.com>
2025-03-02 18:18:51 +0300

    first commit

--- client/src/components/dashboard/AdminDashboard.jsx
+++ client/src/components/dashboard/AdminDashboard.jsx
@@ -91,7 +91,7 @@ class AdminDashboard extends Component {
                     Created At
                   </th>
                   <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                    Actions
+                    Action
                   </th>
                 </tr>
               </thead>
